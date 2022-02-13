93e91a564952b2bea41b2771acdf2878b52944f4
nehorowo <trianglek4nz@gmail.com>
2022-02-13 19:21:51 +0300

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+async function getResponses() {
+  let usersResponse = await fetch('http://jsonplaceholder.typicode.com/users');
+  let usersContent = await usersResponse.json();
+  usersContent = usersContent.splice(0, 10);
+
+  let postsResponse = await fetch('http://jsonplaceholder.typicode.com/posts');
+  let postsContent = await postsResponse.json();
+
+  //for in
+  for (key in usersContent) {
+      //posts
+      const filteredPosts = postsContent.filter((post)=>post.userId == usersContent[key].id);
+      usersContent[key].posts = filteredPosts;
+      
+      //title crop
+      usersContent[key].posts.forEach((post)=>{
+        post.title_crop = post.title.slice(0,20) + '...';
+        delete post.userId;
+      })
+
+      //website
+      usersContent[key].website = 'https://' + usersContent[key].website;
+
+      //address
+      const addressValues = Object.values(usersContent[key].address);
+      const [city, street, suite] = addressValues;
+      usersContent[key].address = [city, street, suite].join(', ');
+
+      //company => field 'name'
+      const [name] = Object.values(usersContent[key].company);
+      usersContent[key].company = name;
+      
+      //deleting phone and username
+      delete usersContent[key].phone;
+      delete usersContent[key].username;
+
+      //add comments to Ervin's posts
+
+      console.log(usersContent[key]);
+  }
+  usersContent.forEach((user)=>{
+    if (user.name == 'Ervin Howell') {
+      console.log(user.name)
+      user.posts.forEach((post)=>{
+        let postId = post.id;
+        console.log(postId)
+      })
+    }
+  })
+  
+}
+
+getResponses();
